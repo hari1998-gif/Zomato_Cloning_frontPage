dc8575d8addc45815ff4c61306263b3e84704cba
Hari <71458568+hari1998-gif@users.noreply.github.com>
2022-10-28 00:22:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function breakfast()
+{
+    alert('Readying Breakfast...');
+}
+function lunch()
+{
+    alert('Readying Lunch...');
+}
+function dinner()
+{
+    alert('Readying Dinner...');
+}
+function snacks()
+{
+    alert('Readying Snacks...');
+}
+function drinks()
+{
+    alert('Readying Drinks...');
+}
+function party()
+{
+    alert('Ready for Night party...');
+}
